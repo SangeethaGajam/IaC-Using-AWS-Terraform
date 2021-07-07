429730bc3f747125af0fc56b3df4adc1834bcd56
SangeethaGajam <64967673+SangeethaGajam@users.noreply.github.com>
2021-07-06 23:57:36 -0400

    Update var.tf
    Added nsg_rule

--- var.tf
+++ var.tf
@@ -1,14 +1,31 @@
 variable "instance_count" {
-  type = number
-  default = "2"
+  type        = number
+  description = "No of EC2 instance to be created"
 }
 
 variable "instance_type" {
-  default = "t2.micro"
+  description = "Type of EC2 Instance"
 }
 
 variable "key_name" {
   description = "key name for the instance"
-  default = "main-key"
 }
 
+//Provider
+variable "access_key" {
+  description = "AWS Access key"
+}
+
+variable "secret_key" {
+  description = "AWS Secret Key"
+}
+
+variable "region" {
+  description = "AWS Region"
+}
+
+//NSG
+variable "nsg_rule" {
+  type        = list(list(string))
+  description = "Security Group Description and Port"
+}
