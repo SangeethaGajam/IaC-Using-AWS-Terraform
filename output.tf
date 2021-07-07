2acd03369243c6c5a2971aaef13a15367fac4283
SangeethaGajam <64967673+SangeethaGajam@users.noreply.github.com>
2021-07-06 23:57:00 -0400

    Update output.tf
    Added load balancer dns name

--- output.tf
+++ output.tf
@@ -1,21 +1,28 @@
-# Outputs Public ip of webserver1
+# Outputs Public ip of webserver0
 output "Webserver-Public-IP-1" {
-  value = aws_instance.webserver1.public_ip
+  value = aws_instance.webserver.0.public_ip
 }
 
-# Outputs Public ip of webserver2
+# Outputs Public ip of webserver1
 output "Webserver-Public-IP-2" {
-  value = aws_instance.webserver2.public_ip
+  value = aws_instance.webserver.1.public_ip
+}
+
+
+# Outputs EC2 Instance state /webserver0
+output "instance_state1" {
+  description = "List of instance states of instances"
+  value       = aws_instance.webserver.0.instance_state
 }
 
 # Outputs EC2 Instance state /webserver1
-output "instance_state" {
+output "instance_state2" {
   description = "List of instance states of instances"
-  value       = aws_instance.webserver1.instance_state
+  value       = aws_instance.webserver.1.instance_state
 }
 
-# Outputs EC2 Instance state /webserver2
-output "instance_state" {
-  description = "List of instance states of instances"
-  value       = aws_instance.webserver2.instance_state
+# Outputs Load Balancer DNS Name
+output "load_balancer_dns" {
+  description = "Load Balancer DNS Name"
+  value       = aws_elb.terraform-elb.dns_name
 }
